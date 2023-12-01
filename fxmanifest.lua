6af408927aeecaa6ee38926a7666afc303f4f5f9
DERPRINCE-SN-Service <140327731+DERPRINCE-SN-Service@users.noreply.github.com>
2023-12-01 13:32:35 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+author 'SN-Service'
+description "TxAdmin-Logs MAde by DERPRINCE"
+version '1.5.0'
+
+server_scripts {
+  'config.lua',
+  'sv_txadmin.lua',
+}
+
+shared_script '@es_extended/imports.lua'
